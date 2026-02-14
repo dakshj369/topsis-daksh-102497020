8038d4b8297a1578865077bee2d1794cde602fc5
daksh jain <dakshj2090@gmail.com>
2026-02-15 02:27:18 +0530

    test

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+print("MAIL USER:", os.environ.get("MAIL_USERNAME"))
 
 mail = Mail(app)
 # ---------------------------------------------
